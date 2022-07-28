ca63e269edde7357bb5fa10e190a86f44a5aa129
Sam5196 <sammedpachore1008@gmail.com>
2022-07-28 11:57:11 +0530

    Added:Ability to Create Contact in Address Book.

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,7 +1,11 @@
 package com.bridgelabz;
 
 public class AddressBook {
-        public static void main(String[] args) {
-            System.out.println("Welcome to Address Book Program");
-        }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Programs");
+        Contact contact = new Contact("Sammed", "Pachore", "Sangli City", "Sangli", 416416, "Maharashtra", "9145051892",
+                "sammedpachore1008@gmail.com");
+        System.out.println(contact);
+
+    }
 }
